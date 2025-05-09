80c1a8fe27663f19db4838b0aaa50a2b4480ee42
Jamesliu <fly2000@gmail.com>
2025-05-09 17:29:02 +0800

    feat(ui): 新增 PLC 連接的驗證與錯誤提示功能

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,121 +38,249 @@
 			this.btnConnect = new System.Windows.Forms.Button();
 			this.btnRead = new System.Windows.Forms.Button();
 			this.btnWrite = new System.Windows.Forms.Button();
+			this.lblIp = new System.Windows.Forms.Label();
+			this.lblPort = new System.Windows.Forms.Label();
+			this.lblPlcType = new System.Windows.Forms.Label();
+			this.lblAddress = new System.Windows.Forms.Label();
+			this.lblDataType = new System.Windows.Forms.Label();
+			this.lblResult = new System.Windows.Forms.Label();
+			this.lblWriteValue = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// txtIp
 			// 
-			this.txtIp.Location = new System.Drawing.Point(20, 20);
+			this.txtIp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+			this.txtIp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtIp.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.txtIp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.txtIp.Location = new System.Drawing.Point(30, 30);
 			this.txtIp.Name = "txtIp";
-			this.txtIp.Size = new System.Drawing.Size(120, 22);
+			this.txtIp.Size = new System.Drawing.Size(120, 26);
 			this.txtIp.TabIndex = 0;
 			// 
 			// txtPort
 			// 
-			this.txtPort.Location = new System.Drawing.Point(150, 20);
+			this.txtPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+			this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtPort.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.txtPort.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.txtPort.Location = new System.Drawing.Point(170, 30);
 			this.txtPort.Name = "txtPort";
-			this.txtPort.Size = new System.Drawing.Size(60, 22);
+			this.txtPort.Size = new System.Drawing.Size(80, 26);
 			this.txtPort.TabIndex = 1;
 			// 
 			// cmbPlcType
 			// 
-			this.cmbPlcType.FormattingEnabled = true;
+			this.cmbPlcType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+			this.cmbPlcType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbPlcType.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.cmbPlcType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
 			this.cmbPlcType.Items.AddRange(new object[] {
             "Mitsubishi",
             "Siemens",
             "AB",
             "Omron"});
-			this.cmbPlcType.Location = new System.Drawing.Point(220, 20);
+			this.cmbPlcType.Location = new System.Drawing.Point(270, 30);
 			this.cmbPlcType.Name = "cmbPlcType";
-			this.cmbPlcType.Size = new System.Drawing.Size(100, 22);
+			this.cmbPlcType.Size = new System.Drawing.Size(120, 27);
 			this.cmbPlcType.TabIndex = 2;
-			this.cmbPlcType.SelectedIndex = 0;
 			// 
 			// txtAddress
 			// 
-			this.txtAddress.Location = new System.Drawing.Point(20, 60);
+			this.txtAddress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+			this.txtAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtAddress.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.txtAddress.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.txtAddress.Location = new System.Drawing.Point(30, 80);
 			this.txtAddress.Name = "txtAddress";
-			this.txtAddress.Size = new System.Drawing.Size(120, 22);
-			this.txtAddress.TabIndex = 3;
+			this.txtAddress.Size = new System.Drawing.Size(120, 26);
+			this.txtAddress.TabIndex = 4;
 			// 
 			// cmbDataType
 			// 
-			this.cmbDataType.FormattingEnabled = true;
+			this.cmbDataType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+			this.cmbDataType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbDataType.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.cmbDataType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
 			this.cmbDataType.Items.AddRange(new object[] {
-            "Bit",
-            "Word",
-            "DWord",
-            "Float",
-            "Double",
-            "String"});
-			this.cmbDataType.Location = new System.Drawing.Point(150, 60);
+            ConnectPLC.PlcDataType.Bit,
+            ConnectPLC.PlcDataType.Word,
+            ConnectPLC.PlcDataType.DWord,
+            ConnectPLC.PlcDataType.Float,
+            ConnectPLC.PlcDataType.Double,
+            ConnectPLC.PlcDataType.String});
+			this.cmbDataType.Location = new System.Drawing.Point(170, 80);
 			this.cmbDataType.Name = "cmbDataType";
-			this.cmbDataType.Size = new System.Drawing.Size(100, 22);
-			this.cmbDataType.TabIndex = 4;
-			this.cmbDataType.SelectedIndex = 0;
+			this.cmbDataType.Size = new System.Drawing.Size(120, 27);
+			this.cmbDataType.TabIndex = 5;
 			// 
 			// txtResult
 			// 
-			this.txtResult.Location = new System.Drawing.Point(20, 100);
+			this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+			this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtResult.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.txtResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			this.txtResult.Location = new System.Drawing.Point(30, 130);
 			this.txtResult.Name = "txtResult";
 			this.txtResult.ReadOnly = true;
-			this.txtResult.Size = new System.Drawing.Size(200, 22);
-			this.txtResult.TabIndex = 5;
+			this.txtResult.Size = new System.Drawing.Size(250, 26);
+			this.txtResult.TabIndex = 7;
 			// 
 			// txtWriteValue
 			// 
-			this.txtWriteValue.Location = new System.Drawing.Point(230, 100);
+			this.txtWriteValue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+			this.txtWriteValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtWriteValue.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.txtWriteValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.txtWriteValue.Location = new System.Drawing.Point(300, 130);
 			this.txtWriteValue.Name = "txtWriteValue";
-			this.txtWriteValue.Size = new System.Drawing.Size(90, 22);
-			this.txtWriteValue.TabIndex = 6;
+			this.txtWriteValue.Size = new System.Drawing.Size(100, 26);
+			this.txtWriteValue.TabIndex = 8;
 			// 
 			// btnConnect
 			// 
-			this.btnConnect.Location = new System.Drawing.Point(340, 20);
+			this.btnConnect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			this.btnConnect.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnConnect.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.btnConnect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.btnConnect.Location = new System.Drawing.Point(410, 30);
 			this.btnConnect.Name = "btnConnect";
-			this.btnConnect.Size = new System.Drawing.Size(80, 22);
-			this.btnConnect.TabIndex = 7;
+			this.btnConnect.Size = new System.Drawing.Size(90, 28);
+			this.btnConnect.TabIndex = 3;
 			this.btnConnect.Text = "連線";
-			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.UseVisualStyleBackColor = false;
 			this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
 			// 
 			// btnRead
 			// 
-			this.btnRead.Location = new System.Drawing.Point(340, 60);
+			this.btnRead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(200)))), ((int)(((byte)(140)))));
+			this.btnRead.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(200)))), ((int)(((byte)(140)))));
+			this.btnRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnRead.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.btnRead.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.btnRead.Location = new System.Drawing.Point(310, 80);
 			this.btnRead.Name = "btnRead";
-			this.btnRead.Size = new System.Drawing.Size(80, 22);
-			this.btnRead.TabIndex = 8;
+			this.btnRead.Size = new System.Drawing.Size(90, 28);
+			this.btnRead.TabIndex = 6;
 			this.btnRead.Text = "讀取";
-			this.btnRead.UseVisualStyleBackColor = true;
+			this.btnRead.UseVisualStyleBackColor = false;
 			this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
 			// 
 			// btnWrite
 			// 
-			this.btnWrite.Location = new System.Drawing.Point(340, 100);
+			this.btnWrite.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+			this.btnWrite.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+			this.btnWrite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnWrite.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.btnWrite.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.btnWrite.Location = new System.Drawing.Point(410, 130);
 			this.btnWrite.Name = "btnWrite";
-			this.btnWrite.Size = new System.Drawing.Size(80, 22);
+			this.btnWrite.Size = new System.Drawing.Size(90, 28);
 			this.btnWrite.TabIndex = 9;
 			this.btnWrite.Text = "寫入";
-			this.btnWrite.UseVisualStyleBackColor = true;
+			this.btnWrite.UseVisualStyleBackColor = false;
 			this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+			// 
+			// lblIp
+			// 
+			this.lblIp.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblIp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblIp.Location = new System.Drawing.Point(30, 8);
+			this.lblIp.Name = "lblIp";
+			this.lblIp.Size = new System.Drawing.Size(80, 20);
+			this.lblIp.TabIndex = 0;
+			this.lblIp.Text = "IP位址：";
+			// 
+			// lblPort
+			// 
+			this.lblPort.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblPort.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblPort.Location = new System.Drawing.Point(170, 8);
+			this.lblPort.Name = "lblPort";
+			this.lblPort.Size = new System.Drawing.Size(60, 20);
+			this.lblPort.TabIndex = 1;
+			this.lblPort.Text = "Port：";
+			// 
+			// lblPlcType
+			// 
+			this.lblPlcType.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblPlcType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblPlcType.Location = new System.Drawing.Point(270, 8);
+			this.lblPlcType.Name = "lblPlcType";
+			this.lblPlcType.Size = new System.Drawing.Size(80, 20);
+			this.lblPlcType.TabIndex = 2;
+			this.lblPlcType.Text = "PLC型號：";
+			// 
+			// lblAddress
+			// 
+			this.lblAddress.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblAddress.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblAddress.Location = new System.Drawing.Point(30, 58);
+			this.lblAddress.Name = "lblAddress";
+			this.lblAddress.Size = new System.Drawing.Size(80, 20);
+			this.lblAddress.TabIndex = 3;
+			this.lblAddress.Text = "PLC位址：";
+			// 
+			// lblDataType
+			// 
+			this.lblDataType.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblDataType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblDataType.Location = new System.Drawing.Point(170, 58);
+			this.lblDataType.Name = "lblDataType";
+			this.lblDataType.Size = new System.Drawing.Size(80, 20);
+			this.lblDataType.TabIndex = 4;
+			this.lblDataType.Text = "資料型態：";
+			// 
+			// lblResult
+			// 
+			this.lblResult.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblResult.Location = new System.Drawing.Point(30, 108);
+			this.lblResult.Name = "lblResult";
+			this.lblResult.Size = new System.Drawing.Size(90, 20);
+			this.lblResult.TabIndex = 5;
+			this.lblResult.Text = "讀取結果：";
+			// 
+			// lblWriteValue
+			// 
+			this.lblWriteValue.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.lblWriteValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.lblWriteValue.Location = new System.Drawing.Point(300, 108);
+			this.lblWriteValue.Name = "lblWriteValue";
+			this.lblWriteValue.Size = new System.Drawing.Size(70, 20);
+			this.lblWriteValue.TabIndex = 6;
+			this.lblWriteValue.Text = "寫入值：";
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(562, 333);
-			this.Controls.Add(this.btnWrite);
-			this.Controls.Add(this.btnRead);
-			this.Controls.Add(this.btnConnect);
-			this.Controls.Add(this.txtWriteValue);
-			this.Controls.Add(this.txtResult);
-			this.Controls.Add(this.cmbDataType);
-			this.Controls.Add(this.txtAddress);
-			this.Controls.Add(this.cmbPlcType);
-			this.Controls.Add(this.txtPort);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+			this.ClientSize = new System.Drawing.Size(540, 200);
+			this.Controls.Add(this.lblIp);
+			this.Controls.Add(this.lblPort);
+			this.Controls.Add(this.lblPlcType);
+			this.Controls.Add(this.lblAddress);
+			this.Controls.Add(this.lblDataType);
+			this.Controls.Add(this.lblResult);
+			this.Controls.Add(this.lblWriteValue);
 			this.Controls.Add(this.txtIp);
+			this.Controls.Add(this.txtPort);
+			this.Controls.Add(this.cmbPlcType);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.txtAddress);
+			this.Controls.Add(this.cmbDataType);
+			this.Controls.Add(this.btnRead);
+			this.Controls.Add(this.txtResult);
+			this.Controls.Add(this.txtWriteValue);
+			this.Controls.Add(this.btnWrite);
+			this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+			this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "PLC 通訊";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -170,6 +298,13 @@
 		private System.Windows.Forms.Button btnConnect;
 		private System.Windows.Forms.Button btnRead;
 		private System.Windows.Forms.Button btnWrite;
+		private System.Windows.Forms.Label lblIp;
+		private System.Windows.Forms.Label lblPort;
+		private System.Windows.Forms.Label lblPlcType;
+		private System.Windows.Forms.Label lblAddress;
+		private System.Windows.Forms.Label lblDataType;
+		private System.Windows.Forms.Label lblResult;
+		private System.Windows.Forms.Label lblWriteValue;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -30,8 +30,29 @@ namespace ConnectPLC
 		private void btnConnect_Click(object sender, EventArgs e)
 		{
 			string ip = txtIp.Text.Trim();
-			int port = int.Parse(txtPort.Text.Trim());
-			string plcType = cmbPlcType.SelectedItem.ToString();
+			string portText = txtPort.Text.Trim();
+			string plcType = cmbPlcType.SelectedItem?.ToString();
+
+			if (string.IsNullOrWhiteSpace(ip))
+			{
+				MessageBox.Show("請輸入IP位址");
+				return;
+			}
+			if (!System.Net.IPAddress.TryParse(ip, out _))
+			{
+				MessageBox.Show("IP格式錯誤");
+				return;
+			}
+			if (string.IsNullOrWhiteSpace(portText) || !int.TryParse(portText, out int port) || port < 1 || port > 65535)
+			{
+				MessageBox.Show("請輸入正確的Port (1-65535)");
+				return;
+			}
+			if (string.IsNullOrWhiteSpace(plcType))
+			{
+				MessageBox.Show("請選擇PLC型號");
+				return;
+			}
 			IConnectionStrategy strategy = null;
 			switch (plcType)
 			{
@@ -47,6 +68,9 @@ namespace ConnectPLC
 				case "Omron":
 					strategy = new OmronPlcConnectionStrategy();
 					break;
+				default:
+					MessageBox.Show("不支援的PLC型號");
+					return;
 			}
 			_plcConnector = new PlcConnector(strategy);
 			bool result = _plcConnector.Connect(ip, port);
@@ -55,19 +79,69 @@ namespace ConnectPLC
 
 		private void btnRead_Click(object sender, EventArgs e)
 		{
+			if (_plcConnector == null || !_plcConnector.IsConnected)
+			{
+				MessageBox.Show("請先連線PLC");
+				return;
+			}
 			string address = txtAddress.Text.Trim();
+			if (string.IsNullOrWhiteSpace(address))
+			{
+				MessageBox.Show("請輸入PLC位址");
+				return;
+			}
+			if (cmbDataType.SelectedItem == null)
+			{
+				MessageBox.Show("請選擇資料型態");
+				return;
+			}
 			PlcDataType dataType = (PlcDataType)cmbDataType.SelectedItem;
-			object value = _plcConnector.ReadStatus(address, dataType);
-			txtResult.Text = value.ToString();
+			try
+			{
+				object value = _plcConnector.ReadStatus(address, dataType);
+				txtResult.Text = value?.ToString() ?? string.Empty;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"讀取失敗：{ex.Message}");
+			}
 		}
 
 		private void btnWrite_Click(object sender, EventArgs e)
 		{
+			if (_plcConnector == null || !_plcConnector.IsConnected)
+			{
+				MessageBox.Show("請先連線PLC");
+				return;
+			}
 			string address = txtAddress.Text.Trim();
+			if (string.IsNullOrWhiteSpace(address))
+			{
+				MessageBox.Show("請輸入PLC位址");
+				return;
+			}
+			if (cmbDataType.SelectedItem == null)
+			{
+				MessageBox.Show("請選擇資料型態");
+				return;
+			}
+			string valueText = txtWriteValue.Text.Trim();
+			if (string.IsNullOrWhiteSpace(valueText))
+			{
+				MessageBox.Show("請輸入寫入值");
+				return;
+			}
 			PlcDataType dataType = (PlcDataType)cmbDataType.SelectedItem;
-			object value = txtWriteValue.Text.Trim();
-			bool result = _plcConnector.WriteValue(address, value, dataType);
-			MessageBox.Show(result ? "寫入成功" : "寫入失敗");
+			object value = valueText;
+			try
+			{
+				bool result = _plcConnector.WriteValue(address, value, dataType);
+				MessageBox.Show(result ? "寫入成功" : "寫入失敗");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"寫入失敗：{ex.Message}");
+			}
 		}
 	}
 }
